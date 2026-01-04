da86b119b3612e8e95595484db38b4500a7a4508
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2026-01-04 01:52:21 -0500

    add visualization page

---
+++ pages/visualization/visualization.md
@@ -0,0 +1,11 @@
+# Asset Visualizations
+
+## Folder Size Treemap (by Asset Family)
+
+<|chart|type=treemap|labels={labels}|parents={parents}|values={values}|>
+
+<br/>
+
+## Polycount vs Material Count (per Variant)
+
+<|chart|type=scatter|x={scatter_x}|y={scatter_y}|text={scatter_text}|mode=markers|>
---
+++ pages/visualization/visualization.py
@@ -0,0 +1,19 @@
+from taipy.gui import Markdown
+from ...data.data import load_all
+
+metadata, assets, tree_df, kpis, treemap_data = load_all()
+
+# Treemap data
+labels = treemap_data["labels"]
+parents = treemap_data["parents"]
+values = treemap_data["values"]
+
+# Example: polycount vs material_count chart
+if assets is not None and not assets.empty:
+    scatter_x = list(assets["polycount"])
+    scatter_y = list(assets["material_count"])
+    scatter_text = list(assets["variant_name"])
+else:
+    scatter_x = scatter_y = scatter_text = []
+
+visualization_md = Markdown("pages/visualization/visualization.md")

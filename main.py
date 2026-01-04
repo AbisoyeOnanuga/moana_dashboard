b3b83ab4917f98f23f33e53ef1b050092c9f945e
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2026-01-03 20:29:43 -0500

    update  visualization module import, remove navbar

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from taipy.gui import Gui
 from pages.home.home import home_md
 from pages.table.table import table_md
 from pages.tree.tree import tree_md
-from pages.visualization.visualization import visualization_md
+from pages.visualization import visualization
 from pages.detail.detail import detail_md
 
-navbar = """
-<|menu|lov=/;Table;Tree;Visualization;Detail|>
-"""
 stylekit = {
     "color_primary": "rgb(60, 120, 200)",
     "color_secondary": "rgb(200, 160, 100)",
@@ -19,7 +16,7 @@ pages = {
     "/": home_md,
     "Table": table_md,
     "Tree": tree_md,
-    "Visualization": visualization_md,
+    "Visualization": visualization.visualization_md,
     "Detail": detail_md,
 }
 

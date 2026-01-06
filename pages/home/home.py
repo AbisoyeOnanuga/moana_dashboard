821086ec35dd710f4de72a15644dbd077a4fbbd3
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2026-01-05 19:13:20 -0500

    update props and environment count

--- pages/home/home.py
+++ pages/home/home.py
@@ -5,8 +5,8 @@ from pages.navbar import navbar
 total_assets = kpis["total_assets"]
 total_variants = kpis["total_variants"]
 total_characters = kpis["total_characters"]
-total_props = kpis["total_props"]
-total_environments = kpis["total_environments"]
+total_props = sum(1 for fam in assets["asset_family"] if fam not in ["character", "environment"])
+total_environments = sum(1 for fam in assets["asset_family"] if fam in ["environment"])
 total_materials = kpis["total_materials"]
 
 home_md = Markdown("pages/home/home.md")
